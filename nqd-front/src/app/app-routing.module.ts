2da825ad82929f42f5a9e874410b9742969ab131
David Pignato <david.pignato@enseirb-matmeca.fr>
2020-04-29 15:42:36 +0200

    adding basic routing + error page

--- nqd-front/src/app/app-routing.module.ts
+++ nqd-front/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'route1', component: AccueilComponent },
+  { path: 'route2', component: AccueilComponent },
+  { path: 'route3', component: AccueilComponent },
+  {
+    path        : '**',
+    pathMatch   : 'full',
+    component   : ErrorPageComponent
+} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- nqd-front/src/app/app.module.ts
+++ nqd-front/src/app/app.module.ts
@@ -6,16 +6,22 @@ import { AppComponent } from './app.component';
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { UserService } from './services/user.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccueilComponent
+    AccueilComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
   providers: [
     UserService
