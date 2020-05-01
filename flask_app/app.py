8088a7dee8231d7a9c703f358c4b4d4b0275e468
David Pignato <david.pignato@enseirb-matmeca.fr>
2020-05-01 17:04:28 +0200

    articles put + delete

--- flask_app/app.py
+++ flask_app/app.py
@@ -63,6 +63,31 @@ def get_users_stat():
 
 
 #====================================ARTICLES========================================
+
+@app.route('/articles', methods=['GET'])
+def get_all_articles():
+    articles = articleCollection.find()
+    response = []
+    for article in articles:
+        article['_id'] = str(article['_id'])
+        response.append(article)
+    return json.dumps(response, default = datetime_to_string)
+
+
+
+@app.route('/articles/<id>', methods=['GET'])
+def get_article(id):
+    article = articleCollection.find_one({"_id":ObjectId(id)})
+    response = []
+    if article == None:
+        return ('Article not found', 404)
+    else:
+        article['_id'] = str(article['_id'])
+        response.append(article)
+        return json.dumps(response, default = datetime_to_string)
+
+
+
 @app.route('/articles', methods=['POST'])
 def save_article():
     req_data = request.get_json()
@@ -73,30 +98,35 @@ def save_article():
     return (json.dumps(req_data, default = datetime_to_string), 201)
 
 
+@app.route('/articles', methods=['PUT'])
+def update_article():
+    article = request.get_json()
+    ## TO DO: Verification article conforme
+    if articleCollection.find_one({"_id":ObjectId(article['_id'])}) == None:
+        return ('Article not found', 404)
+    
+    articleCollection.update({"_id":ObjectId(article['_id'])},{'$set':{'title':article['title'], 'category':article['category'], 'description':article['description'], 'content':article['content'] }})  
+    return (json.dumps(article, default = datetime_to_string), 201)
 
-@app.route('/articles', methods=['GET'])
-def get_all_articles():
-    documents = articleCollection .find()
+
+
+@app.route('/articles/<id>', methods=['DELETE'])
+def delete_article(id):
+    deleted_article = articleCollection.remove({"_id":ObjectId(id)})     
     response = []
-    for document in documents:
-        document['_id'] = str(document['_id'])
-        response.append(document)
-    return json.dumps(response, default = datetime_to_string)
-
-
-
-@app.route('/articles/<id>', methods=['GET'])
-def get_article(id):
-    article = articleCollection .find_one({"_id":ObjectId(id)})
-    response = []
-    if article == None:
-        return (response, 404)
+    if deleted_article['n'] == 0:       # n is the number of deleted articles returned by the request
+        return ('Article not found', 404)
     else:
-        article['_id'] = str(article['_id'])
-        response.append(article)
+        response.append(deleted_article)
         return json.dumps(response, default = datetime_to_string)
 
-## DELETE, PUT, COUNT, ...
+
+
+
+
+
+
+
 
 
 
